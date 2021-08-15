e178902af5d820ecf12f0ceee9570e5527522df3
cothis <minho1030@naver.com>
2021-08-15 14:40:01 +0900

    feat: client-backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
+console.log(__dirname);
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({ credentials: true, origin: true }));
-app.get('/', (req, res) => res.send('Hello World'));
+app.use(express.static(`${__dirname}/../build/client`));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   next(new Error('404 Not Found'));
